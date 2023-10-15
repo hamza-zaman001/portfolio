586ffdd6f512edd1c70644bf3cf46739e8886a9c
hamza-zaman001 <hamzabadiuzzamankhan@gmail.com>
2023-10-16 00:28:43 +0500

    bug fixed and updated...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,42 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
+  const [active, setActive] = useState(' ')
+
   return (
-    <div>Navbar</div>
+    <nav
+      className={`${styles.paddingX} w-full items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          {/* logo */}
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Hamza <span className='sm:block hidden '>| Reactronaut</span></p>  
+          {/* link tags */}
+        </Link> 
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title
+                ? "text-white"
+                : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
   )
 }
 
