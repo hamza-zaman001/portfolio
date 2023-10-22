2f8b747952e683d5df542f7d9107abaf6b9bbcd2
hamza-zaman001 <hamzabadiuzzamankhan@gmail.com>
2023-10-22 23:21:24 +0500

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,8 +15,13 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Hamza</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop eye-catching visuals, user <br className='sm:block hidden' />interfaces and web applications
+          </p>
         </div>
       </div>
+      
+        <ComputersCanvas />
     </section>
   )
 }
